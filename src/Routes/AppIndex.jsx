a83bc3261b6b77b6f5a651a983283c26287c5907
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2023-07-05 14:25:47 +0600

    git update

--- src/Routes/AppIndex.jsx
+++ src/Routes/AppIndex.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from '../Pages/HomePage/HomePage';
 import ErrorPage from '../Pages/Errorpage/ErrorPage';
 import ContactPage from '../Pages/ContactPage/ContactPage';
@@ -10,14 +10,14 @@ import AppNavbar from '../Layout/AppNavbar';
 const AppIndex = () => {
   return (
     <BrowserRouter>
-    <AppNavbar/>
-    <Routes>
-    <Route path='/' element={<HomePage/>}/>
-    <Route path='/projects' element={<ProjectPage/>}/>
-    <Route path='/resume' element={<ResumePage/>}/>
-    <Route path='/contact' element={<ContactPage/>}/>
-    <Route path='*' element={<ErrorPage/>}/>
-    </Routes>
+      <AppNavbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/projects' element={<ProjectPage />} />
+        <Route path='/resume' element={<ResumePage />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
     </BrowserRouter>
   )
 }
