f41a63308d78df7879dc9eda3c14b569f819b960
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2023-07-10 01:04:04 +0600

    add help code website

--- src/Components/ProjectArray.js
+++ src/Components/ProjectArray.js
@@ -1,5 +1,6 @@
 import BarImage from './Images/BarApp.png'
 import PortFolioX from './Images/MyP.png'
+import HelpCode from './Images/Helpcode.jpg'
 
 export const ProjectArr = [
     {
@@ -13,13 +14,13 @@ export const ProjectArr = [
         language:["JavaScript","HTML","CSS","React"],
     },{
         id:2,
-        name:"Bar Website",
-        title:"The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.",
-        link:"#",
-        image:"https://cdn.pixabay.com/photo/2023/06/19/11/10/hay-8074152_1280.jpg",
-        tightColor:"#2AB090",
-        liteColor:"#E0FFF8",
-        language:["JavaScript","HTML","CSS","React"],
+        name:"HC0DE",
+        title:"its only for my help",
+        link:"helpcode.netlify.app",
+        image:HelpCode,
+        tightColor:"#ff9d00",
+        liteColor:"#ffdca3",
+        language:["HTML","CSS","React"],
     },
     {
         id:3,
